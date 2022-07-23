b885fd238ebdc084a7f469caf5659fc05c4f3f7d
Pavel Hofman <pavel.hofman@ivitera.com>
2022-07-23 09:55:44 +0200

    C files moved to src dir

--- src/compile_amd64.sh
+++ src/compile_amd64.sh
@@ -18,7 +18,7 @@ BASEDIR=$(dirname "$0")
 rm $BASEDIR/*.o $BASEDIR/*.so
 
 for FILE in jni_iface impl ; do
-  gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux  $BASEDIR/$FILE.c -o $BASEDIR/$FILE.o
+  gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I$BASEDIR/../ $BASEDIR/$FILE.c -o $BASEDIR/$FILE.o
 done
 
 gcc -shared -Wl,--hash-style=both -Wl,-z,defs -Wl,-O1 -m64 -Wl,-z,noexecstack -Wl,--exclude-libs,ALL -Wl,-z,origin -Wl,-rpath,\$ORIGIN -Wl,-soname=libcsjsound_amd64.so $BASEDIR/*.o -o $BASEDIR/libcsjsound_${JAVA_OS_ARCH}.so -lasound
